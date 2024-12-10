de3630a003ae1e9e541668971d742298d661afaa
Max Derbenwick <maximusderby@gmail.com>
2024-12-10 17:35:22 +0100

    Added LSPs

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -42,7 +42,7 @@ end
 
 require('mason-lspconfig').setup({
    -- Add LSPs to install here
-   ensure_installed = { 'clangd', 'pylsp', 'lua_ls', 'rust_analyzer' }
+   ensure_installed = { 'clangd', 'pylsp', 'lua_ls', 'rust_analyzer', 'jdtls', 'ts_ls' }
 })
 
 ------------------------
@@ -53,3 +53,5 @@ require('lsps.clangd')(lspconfig, on_attach)
 require('lsps.pylsp')(lspconfig, on_attach)
 require('lsps.lua_ls')(lspconfig, on_attach)
 require('lsps.rust_analyzer')(lspconfig, on_attach)
+require('lsps.jdtls')(lspconfig, on_attach)
+require('lsps.ts_ls')(lspconfig, on_attach)
---
+++ lua/lsps/jdtls.lua
@@ -0,0 +1,5 @@
+return function(lspconfig, on_attach)
+   lspconfig.jdtls.setup({
+      on_attach = on_attach
+   })
+end
