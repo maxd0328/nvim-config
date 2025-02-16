7d817c018dfcf83064d6ac8d36b34e861775d2cf
Max Derbenwick <maximusderby@gmail.com>
2025-02-16 18:04:23 +0100

    New typescript LSP

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -42,7 +42,7 @@ end
 
 require('mason-lspconfig').setup({
    -- Add LSPs to install here
-   ensure_installed = { 'clangd', 'pylsp', 'lua_ls', 'rust_analyzer', 'jdtls', 'ts_ls' }
+   ensure_installed = { 'clangd', 'pylsp', 'lua_ls', 'rust_analyzer', 'jdtls', 'vtsls' }
 })
 
 ------------------------
@@ -54,4 +54,4 @@ require('lsps.pylsp')(lspconfig, on_attach)
 require('lsps.lua_ls')(lspconfig, on_attach)
 require('lsps.rust_analyzer')(lspconfig, on_attach)
 require('lsps.jdtls')(lspconfig, on_attach)
-require('lsps.ts_ls')(lspconfig, on_attach)
+require('lsps.vtsls')(lspconfig, on_attach)
